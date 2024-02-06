e05000040239a6deaf2d8e5fc9968cd7a3fe29c2
LuisD <luis.domiciano@hotmail.com>
2024-02-06 15:20:16 -0300

    feat: user domain

---
+++ src/main/java/com/luisd/restapi/domain/UserDomain.java
@@ -0,0 +1,85 @@
+package com.luisd.restapi.domain;
+
+import java.io.Serializable;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity(name = "user")
+public class UserDomain implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotEmpty(message = "name field is mandatory")
+    @Length(min = 3, max = 100, message = "Size must be between 3 and 100 characters")
+    private String name;
+    @NotEmpty(message = "login field is mandatory")
+    private String login;
+    @NotEmpty(message = "password field is mandatory")
+    private String password;
+    public UserDomain() {
+    }
+    public UserDomain(Integer id, String name, String login, String password) {
+        this.id = id;
+        this.name = name;
+        this.login = login;
+        this.password = password;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        UserDomain other = (UserDomain) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+    @Override
+    public String toString() {
+        return "UserDomain [id=" + id + ", name=" + name + ", login=" + login + ", password=" + password + "]";
+    }
+     
+}
